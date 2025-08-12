d65384987dc212ec74f06b5e4cf9311f2bb7989e
mdsam7773 <59015126+mdsam7773@users.noreply.github.com>
2025-08-12 16:44:56 +0530

    Add files via upload

---
+++ studyapp/base/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from django.http import HttpResponse
+from . import views
+
+
+urlpatterns = [
+
+
+    path('login/', views.LoginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+    path('' , views.home , name = 'home'),
+    path('room/<str:pk>/', views.room , name = 'room'),
+    path('profile/<str:pk>/', views.userProfile, name = 'user-profile'),
+
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>',views.updateRoom,name='update-room'),
+    path('delete-message/<str:pk>',views.deleteMessage,name='delete-message'),
+    path('delete-room/<str:pk>',views.deleteRoom,name='delete-room'),
+    path('update-user/',views.updateUser,name='update-user'),
+    path('topics/',views.topicsPage,name='topics'),
+    path('activity/',views.activityPage,name='activity'),
+
+]
